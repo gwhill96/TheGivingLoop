f3d56ec8723d722f5a23d8ff449bc6bf99720160
Louise Stone <louisemstone@hotmail.com>
2022-11-29 13:01:02 +0000

    removed double of devise users

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-
-  devise_for :users
   resources :baskets do
     resources :basket_items
   end
