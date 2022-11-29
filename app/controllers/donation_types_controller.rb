b88d9f2e276a7dae42c8d3623643ee12a8084117
George Hill <gwhill96@gmail.com>
2022-11-29 17:56:19 +0000

    Donations views

--- app/controllers/donation_types_controller.rb
+++ app/controllers/donation_types_controller.rb
@@ -3,38 +3,38 @@ class DonationTypesController < ApplicationController
     @donationtypes = DonationType.where(params[:user_id])
   end
 
-  # def show
-  #   @donationtype = DonationType.find(params[:id])
-  # end
+  def show
+    @donationtype = DonationType.find(params[:id])
+  end
 
   def new
     @donationtype = DonationType.new
   end
 
-  # def create
-  #   @donationtype = DonationType.new(donationtype_params)
-  #   if @donationtype.save!
-  #     redirect_to user_path(current_user)
-  #   else
-  #     render :new, status: :unprocessable_entity
-  #   end
-  # end
+  def create
+    @donationtype = DonationType.new(donationtype_params)
+    if @donationtype.save!
+      redirect_to user_path(current_user)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
-  # def edit
-  #   @donationtype = DonationType.find(params[:id])
-  # end
+  def edit
+    @donationtype = DonationType.find(params[:id])
+  end
 
-  # def update
-  #   @donationtype = DonationType.find(params[:id])
-  #   @donationtype.update(donationtype_params)
-  #   redirect_to donationtype_path(@donationtype)
-  # end
+  def update
+    @donationtype = DonationType.find(params[:id])
+    @donationtype.update(donationtype_params)
+    redirect_to donationtype_path(@donationtype)
+  end
 
-  # def destroy
-  #   @donationtype = DonationType.find(params[:id])
-  #   @donationtype.destroy
-  #   redirect_to user_path(current_user), status: :see_other
-  # end
+  def destroy
+    @donationtype = DonationType.find(params[:id])
+    @donationtype.destroy
+    redirect_to user_path(current_user), status: :see_other
+  end
 
   private
 
