282b91077a56ddb0cb34cb47924aad5bab403428
George Hill <gwhill96@gmail.com>
2022-12-06 14:47:10 +0000

    More maybe fixes?

--- app/javascript/controllers/basket_subscription_controller.js
+++ app/javascript/controllers/basket_subscription_controller.js
@@ -3,13 +3,20 @@ import { createConsumer } from "@rails/actioncable"
 
 // Connects to data-controller="basket-subscription"
 export default class extends Controller {
-  static values = { basketId: Number }
-  static targets = ["basket"]
+//   static values = { basketId: Number }
+
+  static get values() {
+    return { basketId: Number }
+  }
+
+  static get targets() {
+    return ["basket"]
+  }
   connect() {
     this.channel = createConsumer().subscriptions.create(
       { channel: "BasketChannel", id: this.basketIdValue },
       { received: data => this.basketTarget.innerHTML = data }
     )
-    console.log(`Subscribe to the basket with the id ${this.basketIdValue}.`)
-    }
+    // console.log(`Subscribe to the basket with the id ${this.basketIdValue}.`)
   }
+}
