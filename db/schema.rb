877c27f9afc0b612f3f34ec16541b4b08df43aee
Katherine Brown <katherine.brown06@outlook.com>
2022-12-06 16:32:49 +0000

    index cards updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_05_151511) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_06_113831) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_151511) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price_cents", default: 0, null: false
+    t.string "bio"
+    t.string "photo_url"
     t.index ["charity_profile_id"], name: "index_donation_types_on_charity_profile_id"
   end
 
