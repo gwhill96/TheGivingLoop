c268aa41365af5cc5730fcd5384f7c5a812d1765
Louise Stone <louisemstone@hotmail.com>
2022-12-08 10:26:18 +0000

    updated code so that most recent donations are at the top

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
-    @donation_types = DonationType.all
+    @donation_types = DonationType.order(created_at: :desc)
     authorize @user
   end
 
